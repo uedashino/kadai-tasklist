179ad846eeaa83fa46ed234b1cb03eee07d0d752
shino.ueda <shino_ueda@r.recruit.co.jp>
2023-04-27 11:27:20 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,4 @@
 class UsersController < ApplicationController
-  def index
-    @pagy, @users = pagy(User.order(id: :desc), items: 25)
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
 
   def new
     @user = User.new
@@ -13,18 +6,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
+    
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
   end
-
+  
   private
-
+  
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
