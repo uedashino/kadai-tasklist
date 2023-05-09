adb16cf1c14a1ec9465f0f2440b179b8dfec6476
shino.ueda <shino_ueda@r.recruit.co.jp>
2023-05-09 03:19:16 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,9 +3,7 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    if logged_in?
-      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc), items: 3)
-    end
+    @pagy, @tasks = pagy(current_user.tasks.order(id: :desc), items: 3)
   end
 
   def show
